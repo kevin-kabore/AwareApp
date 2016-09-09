02dffc7b0f6b39d4fbc4969351e2b3f40209cd94
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-09-09 00:40:13 -0700

    last minute stuff

--- frontEnd/js/app.routes.js
+++ frontEnd/js/app.routes.js
@@ -11,13 +11,19 @@
     $stateProvider
       .state('homePage', {
         url: '/',
-        templateUrl: '../templates/home.html'
+        templateUrl: './templates/home.html'
       })
       .state('messages', {
         url: '/messages',
-        templateUrl: '../templates/messages.html',
+        templateUrl: './templates/messages.html',
         controller: 'MessagesController',
         controllerAs: 'messageCtrl'
+      })
+      .state('mymessages', {
+        url: '/mymessages',
+        templateUrl: '../templates/mymessages.html',
+        controller: 'MyMessagesController',
+        controllerAs: 'myMessageCtrl'
       })
       .state('signup', {
         url: '/signup',
